b6a2b8dcfe8b9c4076b3d2f608288d08a842cd52
Donghwi Kim <wheeking@gmail.com>
2020-02-05 22:02:00 +0900

    pull

--- algorithm/src/baekjoon/B1260_DFS_BFS.java
+++ algorithm/src/baekjoon/B1260_DFS_BFS.java
@@ -1,5 +1,6 @@
 package baekjoon;
 
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
@@ -26,20 +27,12 @@ public class B1260_DFS_BFS {
 		dfs(map, v);
 		System.out.println();
 		bfs(map, v);
-		
-//		
-//		for (int i = 0; i < map.length; i++) {
-//			for (int j = 0; j < map.length; j++) {
-//				System.out.print(map[i][j] + " ");
-//			}
-//			System.out.println();
-//		}
 	}
 	
 	public static void bfs(int[][] map, int v) {
 		int[] visited = new int[map.length];
 		Queue<Integer> q = new LinkedList<Integer>();
-		
+
 		q.offer(v);	// 시작정점 큐에 삽입
 		visited[v] = 1;	// 방문한 것으로 표시
 		while(!q.isEmpty()) {
@@ -54,10 +47,12 @@ public class B1260_DFS_BFS {
 			}
 		}
 	}
+
+
 	
 	public static void dfs(int[][] map, int v) {
 		int[] visited = new int[map.length];
-		int[] stack = new int[map.length +20];
+		int[] stack = new int[map.length];
 		int top = -1;	// 스택 생성
 		
 		stack[++top] = v;	// 시작 정점 스택에 삽입
@@ -66,22 +61,28 @@ public class B1260_DFS_BFS {
 		while (top > -1) { // 스택이 비워질 때까지
 			int w = -1;	// v 정점에 인접한 정점
 			// 인접한 정점 중 미방문 정점 착기
-			for (int i = 0; i < map.length; i++) {
+			for (int i = 1; i < map.length; i++) {
 				if (map[v][i] == 1 && visited[i] == 0) {
 					w = i; // 다음 갈 정점을 저장
 					stack[++top] = w;	// 스택에 넣기
 					visited[w] = 1;	// 방문 체크
-					
+
 					System.out.print(w + " ");	// 방문해서 할 일.
 					v = w;	// v에 w값(더 깊이 들어가게)
 					break; // 나와서 다시
 				}
 			}
-			
+
+			////////////////////////// ????
 			if (w == -1) {	// 인접한 정점 중 미방문 정점 없을 때
 				// 이전 갈림길로.
-				v = stack[top--];
-			}	
+				// 이전 갈림길은 top-- 가아니고 --top(top-1) 이 가리키고 있음에 유의해라.
+				int temp = --top;
+				if (temp == -1)
+					break;
+				else
+					v = stack[temp];
+			}
 		}
 	}
 }
