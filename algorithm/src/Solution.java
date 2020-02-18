303723e5d7610097fe28d020c2b1026c1860baca
김동휘 <wheeking@gmail.com>
2020-02-18 09:17:08 +0900

    .

--- algorithm/src/Solution.java
+++ algorithm/src/Solution.java
@@ -6,31 +6,9 @@ public class Solution {
 		int T = sc.nextInt();
 		
 		for (int test_case = 1; test_case <= T; test_case++) {
-			
-<<<<<<< HEAD
-			for (int i = 0; i < N; i++) {
-				int command = sc.nextInt();
-				if (command == 0) {
-					distance += nowSpeed*1;
-					continue;
-				}
-				int speed = sc.nextInt();
-				
-				if (command == 1) {
-					nowSpeed += speed;
-				} else {
-					nowSpeed -= speed;
-					if (nowSpeed < 0)
-						nowSpeed = 0;
-				}
-				
-				distance += nowSpeed*1;
-			}
-			
-			System.out.println("#" + test_case + " " + distance);
-=======
+
+	
 			System.out.println("#" + test_case + " ");
->>>>>>> 087c295236f9a84fb2d063c47554fcc39f920c47
 		}
 	}
 }
